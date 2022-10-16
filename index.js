822f4eec3bc0044c3166cdb47bee89a248d4d573
Zoriana <zoriana.shatkovska@gmail.com>
2022-10-16 09:29:22 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    changeText()
+  });
+  
+  function changeText() {
+    document.querySelector('#text').textContent = 'This is really cool!';
+  }
